37149aac61e8e4e08f77fa43a037a127efe47734
zihui829 <57865071+zihui829@users.noreply.github.com>
2019-11-25 14:05:21 +0000

    Update tststs.R

--- tststs.R
+++ tststs.R
@@ -32,16 +32,33 @@ set.seed(443)
 index <- seq(1, 17413)
 test_index <- sample(index, 3483)
 train_index <- index[-test_index]
+
 ## linear regression
 summary(reg_bike <- lm(cnt~.+I(trend^2), data = bike[train_index,]))
 yhat_lm <- predict(reg_bike, bike[test_index,])
 summary((yhat_lm - bike$cnt[test_index])^2)
 
 
+## Ridge
+fit.ridge <-glmnet(bike_x, bike_y[train_index], alpha=0)
+plot(fit.ridge, xvar="lambda", label= TRUE)
+plot(fit.ridge, xvar="dev", label= TRUE)
+cv.ridge <-cv.glmnet(bike_x, bike_y[train_index], alpha=0)
+plot(cv.ridge)
+coef(cv.ridge)
+coef(glmnet(bike_x, bike_y[train_index],alpha=0, lambda=cv.ridge$lambda.min))
+
+grid <-10^seq(10, -2, length=100)
+fit.ridge1 <-glmnet(bike_x, bike_y[train_index],alpha=0, lambda=grid)
+dim(coef(fit.ridge1))
+fit.ridge1$lambda[50]
+coef(fit.ridge1)[,50]
+
+## Ljung-Box tests
 Box.test(reg_bike$residuals, lag = 10, type = c( "Ljung-Box"), fitdf = 0)
 Box.test(bike_y, lag = 10, type = c( "Ljung-Box"), fitdf = 0)
 
-
+## decomposition seasonal adjustment
 bikets<-ts(bike$cnt,frequency=12)
 ts.plot(bikets)
 bikedecomp<-decompose(bikets)
@@ -50,8 +67,10 @@ bikeadj<-bikets-bikedecomp$seasonal
 plot(bikeadj)
 Box.test(bikeadj, lag = 10, type = c( "Ljung-Box"), fitdf = 0)
 
+## stl seasonal adjustment
 bikestl<-stl(bikets,s.window="period")
 plot(bikestl)
 bikestlts<-bikestl$time.series
 Box.test(bikestlts[,1], lag = 10, type = c( "Ljung-Box"), fitdf = 0)
+
 
