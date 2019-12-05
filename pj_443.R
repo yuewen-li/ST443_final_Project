9bc14b5e34f0fd5e7dadbc07ac2370bde16521d8
jqlim <57865018+jqlim@users.noreply.github.com>
2019-12-05 19:27:10 +0000

    Update pj_443.R

--- pj_443.R
+++ pj_443.R
@@ -3,8 +3,9 @@ library(lubridate)
 library(stats)
 library(glmnet)
 library(FNN)
-setwd('D:\\LSE\\ST443 Machine Learning and Data Mining\\pj')
+
 bike_raw <- as.tibble(read.csv('london_merged.csv'))
+
 bike <- bike_raw %>%
   mutate(weather_code = as.factor(weather_code)) %>%
   mutate(is_holiday = as.factor(is_holiday)) %>%
@@ -15,12 +16,12 @@ bike <- bike_raw %>%
   mutate(day = day(date)) %>%
   mutate(month = month(date)) %>%
   mutate(year = year(date)) %>%
-  select(-c(timestamp, date)) %>%
   arrange(t1, t2) %>%
   filter(is.na(month) == F) %>%
-  mutate(trend = seq(1, 17413)) %>%
-  mutate(sin = sin(2*pi*trend/(24*365))) %>%
-  mutate(cos = cos(2*pi*trend/(24*365))) 
+  mutate(yday = yday(date)) %>%
+  mutate(sin = sin(2*pi*yday/(24*365))) %>%
+  mutate(cos = cos(2*pi*yday/(24*365))) %>%
+  select(-c(timestamp,yday,year,month,day,date))
 bike
 bike_y <- bike$cnt
 ## devide testing set and training set
@@ -28,20 +29,27 @@ set.seed(443)
 index <- seq(1, 17413)
 test_index <- sample(index, 3483)
 train_index <- index[-test_index]
+
 ## linear regression
-summary(reg_bike <- lm(cnt~.+I(trend^2), data = bike[train_index,]))
+summary(reg_bike <- lm(cnt~., data = bike[train_index,]))
 yhat_lm <- predict(reg_bike, bike[test_index,])
-summary((yhat_lm - bike$cnt[test_index])^2)
+summary((yhat_lm - bike$cnt[test_index])^2) #MSE 315967
+
+
 ## lasso, first do the variables selection
-bike_x <- model.matrix(cnt~. +I(trend^2), bike[train_index,])[,-1]
+bike_x <- model.matrix(cnt~., bike[train_index,])[,-1] #remove B0
+bike_x
 lasso_cv <- cv.glmnet(bike_x, bike_y[train_index])
+lasso_cv
 coef(lasso_bike <- glmnet(bike_x, bike_y[train_index], lambda = lasso_cv$lambda.1se))
-bike_x_test <- model.matrix(cnt~. +I(trend^2), bike[test_index,])[,-1]
+bike_x_test <- model.matrix(cnt~., bike[test_index,])[,-1]
 yhat_lasso <- predict(lasso_bike, bike_x_test)
 summary((yhat_lasso - bike$cnt[test_index])^2)
+
 ## knn regression
 yhat_knn <- knn.reg(bike_x, bike_x_test, bike_y, k = 10)
-summary((yhat_knn$pred - bike$cnt[test_index])^2)
+summary((yhat_knn$pred - bike$cnt[test_index])^2) #high mse
+
 #######################
 bike_month <- bike %>%
   group_by(month) %>%
@@ -51,9 +59,11 @@ bike_month <- bike %>%
 plot(bike_month$month, bike_month$monthuse)
 
 bike_hour <- bike %>%
-  group_by(time) %>%
+  group_by(time,is_holiday) %>%
   mutate(hourave = mean(cnt)) %>%
-  select(hourave, time) %>%
+  select(hourave, time,is_holiday) %>%
+  group_by() %>%
+  mutate(time=as.numeric(time)) %>%
   unique()
 plot(bike_hour$time, bike_hour$hourave, type = 'p')
 
@@ -62,3 +72,5 @@ bike_day <- bike %>%
   mutate(dayavg = mean(cnt)) %>%
   select(day, dayavg)
 plot(bike_day$day, bike_day$dayavg)  
+ggplot(bike_hour,aes(time,hourave,color=is_holiday))+
+  geom_line()
